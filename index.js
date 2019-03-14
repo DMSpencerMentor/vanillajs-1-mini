1a777f0450c67a4ed644ebc6e76ef66dbd4b2fdc
Spencer Smith <stix1919@hotmail.com>
2019-03-14 13:13:31 -0600

    done. Vanilla is neat

---
+++ index.js
@@ -0,0 +1,30 @@
+const element = document.getElementById("counter")
+
+var count = 0
+
+function increase(){
+    count++
+    element.innerText = count
+    console.log(count)
+}
+function decrease(){
+    count--
+    element.innerText = count
+    console.log(count)
+}
+function reset(){
+    count = 0
+    element.innerHTML = "<mark>" + count + "</mark>"
+    console.log(count)
+}
+
+function selectTheme(theme) {
+    document.getElementsByTagName("body")[0].className = theme
+    document.getElementsByTagName("main")[0].className = theme
+    console.log(theme)
+
+    const buttons = document.getElementsByTagName("button")
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].className = theme
+    }
+}
